f6b36e218c91181b6c9df2e5a09692012e041737
mycodingdesigns <105081784+mycodingdesigns@users.noreply.github.com>
2023-03-09 20:55:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+// Pokemon Info and Stats (From IDs)
 const pokeName = document.querySelector('[pokeName]');
 const pokeImg = document.querySelector('[pokeImg]');
+const pokeId = document.querySelector('[pokeId]');
+const pokeTypes = document.querySelector('[pokeTypes]');
+const pokeAbilities = document.querySelector('[pokeAbilities]');
+const pokeHP = document.querySelector('[pokeHP]');
+const pokeAttack = document.querySelector('[pokeAttack]');
+const pokeDefense = document.querySelector('[pokeDefense]');
+const pokeSpecialAttack = document.querySelector('[pokeSpecialAttack]');
+const pokeSpecialDefense = document.querySelector('[pokeSpecialDefense]');
+const pokeSpeed = document.querySelector('[pokeSpeed]');
 
+// Searching Up A Pokemon
 const search = event => {
   event.preventDefault();
   const {value} = event.target.pokemon;
@@ -10,59 +21,25 @@ const search = event => {
   .catch(err => noPokemon())
 }
 
+// Search Results
+
+
+// Color Coding the Pokemon
+
+
+// Pulling Data From the API
 const outPokemon = data => {
+  console.log(data);
   const sprite = data.sprites.front_default;
   const {stats, types} = data;
   pokeName.textContent = data.name;
-  pokeId.textContent = `Nº ${data.id}`;
   pokeImg.setAttribute('src', sprite);
-
+  pokeId.textContent = `ID Number: ${data.id}`;
+  pokeTypes.textContent = `Types: ${data.types.map(type => type.type.name).join(", ")}`;
+  pokeAbilities.textContent = `Abilities: ${data.abilities.map(ability => ability.ability.name).join(", ")}`;
+  pokeHP.textContent = `Base HP: ${data.stats.find(stat => stat.stat.name === "hp").base_stat}`;
+  pokeAttack.textContent = `Attack: ${data.stats.find(stat => stat.stat.name === "attack").base_stat}`;
+  pokeSpecialAttack.textContent = `Special Attack: ${data.stats.find(stat => stat.stat.name === "special-attack").base_stat}`;
+  pokeSpecialDefense.textContent = `Special Defense: ${data.stats.find(stat => stat.stat.name === "special-defense").base_stat}`;
+  pokeSpeed.textContent = `Speed: ${data.stats.find(stat => stat.stat.name === "speed").base_stat}`;
 }
-
-const noPokemon = () => {
-  pokeName.textContent = 'Pokemon not found. Please check your spelling.';
-  pokeImg.setAttribute('src', '');
-}
-
-// <-----------------------------------Scrapped Code--------------------------------------->
-
-// document.addEventListener('DOMContentLoaded', () => {
-
-// const searchInput = document.getElementById('search-input');
-// const searchResults = document.getElementById('search-results');
-// const searchButton = document.getElementById('search-button');
-// const pokeImg = document.querySelector('[data-poke-img]');
-
-// searchButton.addEventListener('click', () => {
-//     var pokemonName = searchInput.value.toLowerCase().trim();
-//     searchResults.innerHTML = '';
-  
-//     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-//       .then(response => response.json())
-//       .then(data => {
-//         console.log(data);
-//         var pokemon = {
-//           name: data.name,
-//           imageUrl: data.sprites.front_default,
-//           types: data.types.map(type => type.type.name).join(', ')
-//         };
-  
-//         var card = `
-          
-//         `;
-  
-//         searchResults.innerHTML = `
-//             <div>
-//                 <img src="${pokemon.imageUrl}">
-//                 <h2>${pokemon.name}</h2>
-//                 <p>Type(s): ${pokemon.types}</p>
-//             </div>;
-//         `
-//       })
-//       .catch(error => {
-//         searchResults.innerHTML = 'Pokemon not found.';
-//       });
-//   });
-// });
-
-// <-------------------------------------------------------------------------->
