ee21211ad0d98d9b8090780c96f4762d40c48424
Emily Wu <99505073+Emi-lychee@users.noreply.github.com>
2023-03-30 01:25:06 -0400

    Add files via upload

--- all.js
+++ all.js
@@ -29,34 +29,34 @@ const colors = {
 
 for (let i = 1; i <= 898; i++) {
   fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-    .then(response => response.json())
-    .then(data => {
-      const pokemonContainer = document.createElement('div');
-      pokemonContainer.style.display = 'flex';
-      pokemonContainer.style.flexDirection = 'column';
-      pokemonContainer.style.alignItems = 'center';
-      pokemonContainer.style.margin = '3%';
+  .then(response => response.json())
+  .then(data => {
+    const pokemonContainer = document.createElement('div');
+    pokemonContainer.style.display = 'flex';
+    pokemonContainer.style.flexDirection = 'column';
+    pokemonContainer.style.alignItems = 'center';
+    pokemonContainer.style.margin = '3%';
 
-      const pokeName = document.createElement('p');
-      pokeName.textContent = data.name;
+    const pokeName = document.createElement('p');
+    pokeName.textContent = data.name;
 
-      const spriteUrl = data.sprites.front_default;
-      const spriteImg = document.createElement('img');
+    const spriteUrl = data.sprites.front_default;
+    const spriteImg = document.createElement('img');
 
-      const cardColor = pokemonTypes => {
-        const colorType = colors[pokemonTypes[0]];
-        spriteImg.style.background = colorType;
-        spriteImg.style.backgroundSize = '200px 120px';
-        spriteImg.style.borderRadius = '10%';
-      }
+    const cardColor = pokemonTypes => {
+      const colorType = colors[pokemonTypes[0]];
+      spriteImg.style.background = colorType;
+      spriteImg.style.backgroundSize = '200px 120px';
+      spriteImg.style.borderRadius = '10%';
+    }
 
-      spriteImg.src = spriteUrl;
-      pokemonContainer.appendChild(pokeName);
-      pokemonContainer.appendChild(spriteImg);
+    spriteImg.src = spriteUrl;
+    pokemonContainer.appendChild(pokeName);
+    pokemonContainer.appendChild(spriteImg);
 
-      cardColor(data.types.map(type => type.type.name));
-      spriteContainer.appendChild(pokemonContainer);
-    });
+    cardColor(data.types.map(type => type.type.name));
+    spriteContainer.appendChild(pokemonContainer);
+  });
 }
 
 spriteContainer.style.display = 'flex';
